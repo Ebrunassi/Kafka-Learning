36b7d173de5af47e3eb6fa846d0c9451c9b21009
Evandro <ebrunassi@hotmail.com>
2022-08-12 16:59:44 +0200

    Project setup

---
+++ src/test/kotlin/com/learning/kafkastudy/KafkaStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learning.kafkastudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
